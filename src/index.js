15c24a327caad4a94ca8072ba14fbc65672e0d90
jordy muniz <jordy.muniz96@gmail.com>
2021-07-20 16:04:20 -0500

    mosh react code along update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World</h1>;
+// const element = <h1>Hello World</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
+
+//Var is scoped to the function (let is prefered over var)
+//let is scoped to the block({}) it's defined in (const is prefered over let)
+//const variables can't be reassigned
+
+function sayHello(){
+    for (let i = 0; 1 > 5; i++){
+        console.log(i);
+    }
+}
+
+sayHello();
